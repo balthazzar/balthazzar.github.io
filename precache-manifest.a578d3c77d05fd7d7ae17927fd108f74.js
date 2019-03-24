664057acf634d71e286fe349a95121ddf6163d81
Андрей Корешков <akoreshkov@alfabank.ru>
2019-03-24 18:11:03 +0300

    hotfix

--- precache-manifest.a578d3c77d05fd7d7ae17927fd108f74.js
+++ precache-manifest.a578d3c77d05fd7d7ae17927fd108f74.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "c5ce2ec7941099e31f2f",
-    "url": "/static/js/main.67d9ca12.chunk.js"
+    "revision": "5b26b3f22dd060691000",
+    "url": "/static/js/main.ac26da73.chunk.js"
   },
   {
     "revision": "fa5b2a66d46a44186e43",
     "url": "/static/js/2.c4c2f5c1.chunk.js"
   },
   {
-    "revision": "c5ce2ec7941099e31f2f",
+    "revision": "5b26b3f22dd060691000",
     "url": "/static/css/main.0c937116.chunk.css"
   },
   {
-    "revision": "89ad180ccdf89d7ca888aa0749d546d5",
+    "revision": "3257abcd80e12d183599dc3bef3d1e35",
     "url": "/index.html"
   }
 ];
