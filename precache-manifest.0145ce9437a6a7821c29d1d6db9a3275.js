ef047f802a5a786917f98f47588acf1843cea04f
Андрей Корешков <akoreshkov@alfabank.ru>
2019-03-24 17:12:48 +0300

    charts

---
+++ precache-manifest.0145ce9437a6a7821c29d1d6db9a3275.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0ee356e6f81374fc9ad5",
+    "url": "/static/js/main.5f171e19.chunk.js"
+  },
+  {
+    "revision": "fa5b2a66d46a44186e43",
+    "url": "/static/js/2.c4c2f5c1.chunk.js"
+  },
+  {
+    "revision": "0ee356e6f81374fc9ad5",
+    "url": "/static/css/main.0c937116.chunk.css"
+  },
+  {
+    "revision": "f445c53bebdffcace874f2231e5a600b",
+    "url": "/index.html"
+  }
+];
